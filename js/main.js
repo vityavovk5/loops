5c0a54654037320fc9b4e6caf414fbd4fad599f8
vityavovk5 <vityavovk55@gmail.com>
2020-11-16 20:30:23 +0200

    sdfsdf

--- js/main.js
+++ js/main.js
@@ -29,3 +29,36 @@
 // do {
 //     document.write(`${i++}<br>`);
 // } while (i<10);
+
+var a = [1, 2, NaN, 3, 'd', 4, 5, 'a', 'b', null];
+// document.write(a[a.length - 1]);
+
+// for (var i = 0; i< a.length; i++) {   //.length - длина масива
+//     document.write(`${a[i]} <br>`);
+// }
+
+// a.forEach(
+//     function(element) {
+//         document.write(`${element} <br>`);
+//         }
+//     );
+// a.forEach(
+//     function(element) {
+//         if (element != null && (!isNaN(element) || typeof element == 'string')){
+//         document.write(`${element} <br>`);
+//         }
+//     }
+//     );
+
+// for (el in a) {
+//     document.write(`${el} <br>`);
+// }
+// for (el of a) {
+//     document.write(`${el} <br>`);
+// }
+// for (let index in a) {
+//     document.write(`${index} ${a[index]} <br>`);
+// }
+for (let index in a) {
+    document.write(`${index} ${a[index]} <br>`);
+    }
