2bdf568ff79110868f17805ba21b31ec763aa9b0
vityavovk5 <vityavovk55@gmail.com>
2020-11-16 20:10:31 +0200

    tt

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// var i = 0;
+// while (i < 10) {
+
+// if (i==5) {
+//     i++;
+//     continue;
+// }
+ 
+//     document.write(`${i++}<br>`);
+    
+//     if (i==7){
+//         break;
+//     }
+// }
+
+// document.write(0);
+
+// for (var i = 0, j = 10; i<10 && j>3; i++,j--) {
+//     document.write(`${i} ${j}<br>`);
+// }
+
+
+//  for (var i=0; i<10; i++) {
+//     for (var j=0; j<10; j++) {
+//         document.write(`${i} ${j}<br>`);
+//     } 
+//  }
+// var i = 0;
+// do {
+//     document.write(`${i++}<br>`);
+// } while (i<10);
